0f6c2b2407743e89c4cf9cef19eb5ab1bdbe8b6e
Shashwat Sanket <34893157+shashwatsanket997@users.noreply.github.com>
2018-06-12 23:16:11 +0530

    Add files via upload

---
+++ first_process.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 21:06:21 2018
+
+@author: Shashwat Sanket
+"""
+
+#Importing the libraries required
+import pandas as pd
+#import numpy as np
+
+train=pd.read_csv("train.csv",na_values={"Item_Visibility":[0]})
+
+test=pd.read_csv("test.csv",na_values={"Item_Visibility":[0]})
+
+
+train['source']='train'
+
+test['source']='test'
+
+data=pd.concat([train,test],ignore_index=True)
+
+#the one thing we have to focus is item_outlet_Sales
+
+
+discpt=data.describe()
+
+
+#Lets find out how many  zero'es values are
+
+nan_descript=data.apply(lambda x: sum(x.isnull()))
+
+#Now lets find out the unique values in each of the catogorical columns
+
+uniq=data.apply(lambda x: len(x.unique()))
+
+#let do grouping in each catogorical columns
+
+col=["Item_Fat_Content","Item_Type","Outlet_Location_Type","Outlet_Size"]
+
+for i in col:
+    print("The frequency distribution of each catogorical columns is--" + i+"\n")
+    print(data[i].value_counts())   
+
+#Replacing the minimum nan values in the Item_Weight with its mean value
+
+data.fillna({"Item_Weight":data["Item_Weight"].mean()},inplace=True)
+
+#checking the current status of  nan values in the dataframe
+nan_descript=data.apply(lambda x: sum(x.isnull()))
+#Now we 0 nan valuesin Item_Weight
+
+
+data["Outlet_Size"].fillna(method="ffill",inplace=True)
+
+
+nan_descript=data.apply(lambda x: sum(x.isnull()))
+
+
+#Now working on the item_visibility
+
+
+visibilty_avg=data.pivot_table(values="Item_Visibility",index="Item_Identifier")
+
+
+itm_visi=data.groupby('Item_Type')
+
+data_frames=[]
+for item,item_df in itm_visi:
+    data_frames.append(itm_visi.get_group(item))
+for i in data_frames:
+    i["Item_Visibility"].fillna(value=i["Item_Visibility"].mean(),inplace=True)
+    i["Item_Outlet_Sales"].fillna(value=i["Item_Outlet_Sales"].mean(),inplace=True)
+
+new_data=pd.concat(data_frames)
+
+nan_descript=new_data.apply(lambda x: sum(x.isnull()))
+
+#Now we have successfully cleaned our complete dataset.
+new_data["Item_Fat_Content"].replace({'LF':'Low Fat','reg':'Regular','low fat':'Low Fat'},inplace=True)
+
+new_data["Item_Fat_Content"].value_counts()
+
+#Implementing one-hot-Coding method for getting the categorical variables
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+data=new_data
+data['Outlet'] = le.fit_transform(data['Outlet_Identifier'])
+var_mod = ['Item_Fat_Content','Outlet_Location_Type','Outlet_Size','Item_Type','Outlet_Type']
+le = LabelEncoder()
+for i in var_mod:
+    data[i] = le.fit_transform(data[i])
+#One Hot Coding:
+data = pd.get_dummies(data, columns=['Item_Fat_Content','Outlet_Location_Type','Outlet_Size','Outlet_Type',
+                              'Item_Type'])
+
+#Exporting the datas
+
+train = data.loc[data['source']=="train"]
+
+test = data.loc[data['source']=="test"]
+
+#Drop unnecessary columns:
+test.drop(['Item_Outlet_Sales','source'],axis=1,inplace=True)
+#here we are droping the "Item_Outlet_Sales because this only we want to be predicted from the model that we are going to built
+train.drop(['source'],axis=1,inplace=True)
+
+#Export files as modified versions:
+train.to_csv("train_modified.csv",index=False)
+test.to_csv("test_modified.csv",index=False)
+
+#Let's start building the baseline model as  it is non -predicting model and also commenly known as informed guess
+
+#Mean based:
+mean_sales = train['Item_Outlet_Sales'].mean()
+
+#Define a dataframe with IDs for submission:
+base1 = test[['Item_Identifier','Outlet_Identifier']]
+base1['Item_Outlet_Sales'] = mean_sales
+
+#Export submission file
+base1.to_csv("alg0.csv",index=False)
+
+
+'''Very Important Note for creating baseline model
+Seems too naive for you? If you look at the public LB now, you’ll find 4 players below 
+this number. So making baseline models helps in setting a benchmark. 
+If your predictive algorithm is below this, there is something going seriously wrong and \
+you should check your data.'''
+
+#Define target and ID columns:
+target = 'Item_Outlet_Sales'
+IDcol = ['Item_Identifier','Outlet_Identifier']
+
+''' Now from this I have to learn machine learning data_Analytics'''
+import numpy as np
+from sklearn import cross_validation, metrics
+def modelfit(alg, dtrain, dtest, predictors, target, IDcol, filename):
+    #Fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain[target])
+        
+    #Predict training set:
+    dtrain_predictions = alg.predict(dtrain[predictors])
+
+    #Perform cross-validation:
+    cv_score = cross_validation.cross_val_score(alg, dtrain[predictors], dtrain[target], cv=20, scoring='mean_squared_error')
+    cv_score = np.sqrt(np.abs(cv_score))
+    
+    #Print model report:
+    print ("\nModel Report")
+    print ("RMSE : %.4g" % np.sqrt(metrics.mean_squared_error(dtrain[target].values, dtrain_predictions)))
+    print ("CV Score : Mean - %.4g | Std - %.4g | Min - %.4g | Max - %.4g" % (np.mean(cv_score),np.std(cv_score),np.min(cv_score),np.max(cv_score)))
+    
+    #Predict on testing data:
+    dtest[target] = alg.predict(dtest[predictors])
+    
+    #Export submission file:
+    IDcol.append(target)
+    submission = pd.DataFrame({ x: dtest[x] for x in IDcol})
+    submission.to_csv(filename, index=False)
+    
+    
+#Liner Regression model
+print("Creating the models and processing")
+from sklearn.linear_model import LinearRegression, Ridge
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+# print predictors
+alg1 = LinearRegression(normalize=True)
+modelfit(alg1, train, test, predictors, target, IDcol, 'alg1.csv')
+coef1 = pd.Series(alg1.coef_, predictors).sort_values()
+coef1.plot(kind='bar', title='Model Coefficients')
+
+#Ridge Regression Model
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg2 = Ridge(alpha=0.05,normalize=True)
+modelfit(alg2, train, test, predictors, target, IDcol, 'alg2.csv')
+coef2 = pd.Series(alg2.coef_, predictors).sort_values()
+coef2.plot(kind='bar', title='Model Coefficients')
+print("Model has been successfully created and trained. The predicted result is in alg2.csv")
+
+# Decision Tree Model
+
+from sklearn.tree import DecisionTreeRegressor
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg3 = DecisionTreeRegressor(max_depth=15, min_samples_leaf=100)
+modelfit(alg3, train, test, predictors, target, IDcol, 'alg3.csv')
+coef3 = pd.Series(alg3.feature_importances_, predictors).sort_values(ascending=False)
+coef3.plot(kind='bar', title='Feature Importances')
+
+print("Model has been successfully created and trained. The predicted result is in alg3.csv")
+
+
+#Random Forest Model
+
+from sklearn.ensemble import RandomForestRegressor
+
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg5 = RandomForestRegressor(n_estimators=200,max_depth=5, min_samples_leaf=100,n_jobs=4)
+modelfit(alg5, train, test, predictors, target, IDcol, 'alg5.csv')
+coef5 = pd.Series(alg5.feature_importances_, predictors).sort_values(ascending=False)
+coef5.plot(kind='bar', title='Feature Importances')
+
+print("Model has been successfully created and trained. The predicted result is in alg5.csv")
+
+predictors = [x for x in train.columns if x not in [target]+IDcol]
+alg6 = RandomForestRegressor(n_estimators=400,max_depth=6, min_samples_leaf=100,n_jobs=4)
+modelfit(alg6, train, test, predictors, target, IDcol, 'alg6.csv')
+coef6 = pd.Series(alg6.feature_importances_, predictors).sort_values(ascending=False)
+coef6.plot(kind='bar', title='Feature Importances')
+
+print("Model has been successfully created and trained. The predicted result is in alg6.csv")
